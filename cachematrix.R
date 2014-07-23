758b33bb9499b1896fa755d12e062c36c593a9f0
kevin <kevin@kevin-desktop>
2014-07-23 12:12:40 -0400

    added my code

--- cachematrix.R
+++ cachematrix.R
@@ -4,7 +4,24 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    # Cached matrix
+    cachedinv <- NULL
+    # set matrix
+    setmatrix <- function(m) {
+      x <<- m
+      setmatrix <<- NULL
+    }
+    # get the matrix
+    getmatrix <- function() x
+    
+    # set inverse matrix
+    setinvm <- function(inverse) cachedinv <<- inverse
+    # get the inverse matrix
+    getinvm <- function() cachedinv
+    
+    list(setmatrix = setmatrix, getmatrix = getmatrix, setinvm = setinvm, getinvm = getinvm)
+}
+    
 }
 
 
@@ -12,4 +29,18 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  cachedinv <- x$getinvm()
+  
+  # If the inverse is calculated, return it
+  if (!is.null(cachedinv)) {
+    message("get cache")
+    return(cachedinv)
+  }
+  # if not in cache, calculate inverse
+  data <- x$getmatrix()
+  cachedinv <- solve(data, ...)
+  
+  # then cache inv
+  x$setinvm(cachedinv)
+  return(cachedinv)
 }
